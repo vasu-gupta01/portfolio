20a5fb3d891ad43a6163291c4e0c79301545bbd2
vasu-gupta01 <punarvas@ualberta.ca>
2022-08-24 16:51:54 -0600

    bug fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
     return () => {
       window.removeEventListener("scroll", handleShow);
     };
-  });
+  }, [prevScrollY]);
 
   useEffect(() => {
     const handleMouseDown = ({ target }: MouseEvent) => {
