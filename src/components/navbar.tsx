d830f9acd782fdc0a931eca1d91dd0a93c7e8a98
vasu-gupta01 <punarvas@ualberta.ca>
2022-08-31 01:44:21 -0600

    fix theme button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,6 @@
 import { useTheme } from "next-themes";
-import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
-import { SunIcon, MoonIcon } from "@heroicons/react/solid";
 
 const Navbar = () => {
   const [navOpen, setNavOpen] = useState(false);
@@ -16,19 +14,45 @@ const Navbar = () => {
   const handleTheme = () => {
     const currentTheme = theme === "system" ? systemTheme : theme;
 
-    return currentTheme === "dark" ? (
-      <SunIcon
-        className="w-8 h-8 text-yellow-500 transition-all hover:text-yellow-300"
-        role="button"
-        onClick={() => setTheme("light")}
-      />
-    ) : (
-      <MoonIcon
-        className="w-8 h-8 text-gray-900 transition-all hover:text-gray-500"
-        role="button"
-        onClick={() => setTheme("dark")}
-      />
-    );
+    if (currentTheme === "dark") {
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          role="button"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          onClick={() => setTheme("light")}
+          className="w-8 h-8 text-yellow-500 transition-all hover:text-yellow-300"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+          />
+        </svg>
+      );
+    } else {
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-8 h-8 text-gray-900 transition-all hover:text-gray-500"
+          role="button"
+          onClick={() => setTheme("dark")}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+          />
+        </svg>
+      );
+    }
   };
 
   const handleShow = () => {
